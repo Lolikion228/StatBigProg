3adba4b27b618b914454b3cf73284b1980c7ea74
Lolikion228 <st117139@student.spbu.ru>
2026-05-26 14:35:38 +0300

    mb draw pdist is also working

--- big_prog/document.cpp
+++ big_prog/document.cpp
@@ -4,8 +4,8 @@
 
 Document::Document(std::mt19937_64* stdgen):
     _stdgen(stdgen),
-    hist_gen_params(new HistGenParams(stdgen))
-    //pdist_gen_params(new PDistGenParams(stdgen)),
+    hist_gen_params(new HistGenParams(stdgen)),
+    pdist_gen_params(new PDistGenParams(stdgen))
     //draw_time_params(new DrawTimeParams(stdgen))
 {
 
@@ -14,7 +14,7 @@ Document::Document(std::mt19937_64* stdgen):
 
 Document::~Document(){
     delete hist_gen_params;
-    //delete pdist_gen_params;
+    delete pdist_gen_params;
     //delete draw_time_params;
 }
 
