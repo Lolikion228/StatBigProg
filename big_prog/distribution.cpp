4be61cb81bfb56613db688244a0010683cdf9268
Lolikion228 <st117139@student.spbu.ru>
2026-05-30 10:32:29 +0300

    dist construct update

--- big_prog/distribution.cpp
+++ big_prog/distribution.cpp
@@ -62,7 +62,68 @@ const char* Distribution::whoami() const{
 }
 
 
+Distribution::Distribution(const Distribution& other) :
+    _lambda(other._lambda),
+    _capacity(other._capacity),
+    _n_computed(other._n_computed),
+    _computed_probs(nullptr),
+    _computed_cumsums(nullptr)
+{
+    if (other._computed_probs && _capacity > 0) {
+        _computed_probs = new double[_capacity];
+        _computed_cumsums = new double[_capacity];
+        for (int i = 0; i < _n_computed; ++i) {
+            _computed_probs[i] = other._computed_probs[i];
+            _computed_cumsums[i] = other._computed_cumsums[i];
+        }
+    }
+}
+
+
+Distribution::Distribution(Distribution&& other):
+    _lambda(other._lambda),
+    _capacity(other._capacity),
+    _n_computed(other._n_computed),
+    _computed_probs(other._computed_probs),
+    _computed_cumsums(other._computed_cumsums)
+{
+    other._computed_probs = nullptr;
+    other._computed_cumsums = nullptr;
+    other._capacity = 0;
+    other._n_computed = 0;
+}
+
+
+Distribution& Distribution::operator=(const Distribution& other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    delete[] _computed_probs;
+    delete[] _computed_cumsums;
+
+    _lambda = other._lambda;
+    _capacity = other._capacity;
+    _n_computed = other._n_computed;
+
+    if (other._computed_probs && _capacity > 0) {
+        _computed_probs = new double[_capacity];
+        _computed_cumsums = new double[_capacity];
+        for (int i = 0; i < _n_computed; ++i) {
+            _computed_probs[i] = other._computed_probs[i];
+            _computed_cumsums[i] = other._computed_cumsums[i];
+        }
+    }
+    else {
+        _computed_probs = nullptr;
+        _computed_cumsums = nullptr;
+    }
+
+    return *this;
+}
+
+
 Distribution::~Distribution(){
-    delete _computed_cumsums;
-    delete _computed_probs;
+    delete[] _computed_cumsums;
+    delete[] _computed_probs;
 }
--- big_prog/distribution.h
+++ big_prog/distribution.h
@@ -14,11 +14,15 @@ public:
     int _capacity;
 
     Distribution(double lambda);
+    Distribution(const Distribution& other);
+    Distribution(Distribution&& other);
+    Distribution& operator=(const Distribution& other);
+    ~Distribution();
+
     double get_lambda() const;
     void update_probs(int compute_up_to);
     void get_probs(int right_lim, double *p);
     const char* whoami() const;
-    ~Distribution();
 };
 
 #endif // DISTRIBUTION_H
